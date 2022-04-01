1281ffd760aa2edcaff03339ebfe2ba874216f52
Coihue555 <kevlar555@gmail.com>
2022-04-01 11:29:03 -0300

    compatibilidades con dependencias viejas

--- sockets/socket.js
+++ sockets/socket.js
@@ -8,17 +8,21 @@ io.on('connection', client => {
         console.log('Cliente desconectado')
     });
 
+    client.on('connect', () => { 
+        console.log('Cliente conectado de otro')
+    });
+
     client.on('mensaje', (payload)=> {
         console.log('Mensajeeee', payload);
 
         io.emit('mensaje', {admin: 'New msg'} );
     })
 
-    client.on('mi-mensaje', (payload)=> {
+    client.on('emitir-mensaje', (payload)=> {
         console.log('Mensajeeee', payload);
 
-        io.emit('mi-mensaje', 'Heyyy' ); //emite a todos incluido el emisor
-        client.broadcast.emit('mi-mensaje', 'Heyyy' ); //emite a todos menos al emisor
+        io.emit('nuevo-mensaje', payload ); //emite a todos incluido el emisor
+        //client.broadcast.emit('nuevo-mensaje', 'Heyyy' ); //emite a todos menos al emisor
     })
 
   });
